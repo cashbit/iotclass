caff9ee3fc0271b95fe37e2f9976181a094bc244
Carlo Cassinari <carlo@techmakers.io>
2018-04-11 08:52:11 +0200

    u

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('gameStart', function(msg){
+    console.log('gameStart: ' + msg);
+  });
+
+  socket.on('gameStop', function(msg){
+    console.log('gameStop: ' + msg);
+  });
+  
+});
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
