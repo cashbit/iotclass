83cdb9d133825d18897f0b5a66534d2650384de0
Carlo Cassinari <carlo@techmakers.io>
2018-04-11 13:15:10 +0200

    id

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
 
-  console.log('a user connected');
+  console.log('a user connected',socket.id);
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
